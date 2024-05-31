465b54977c2e8edcdc956a200dc26d578e10b891
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2024-05-31 20:14:06 +0600

    ui update

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,10 +1,9 @@
-
 <template>
   <nav class="vue-nav" :style="{backgroundColor:navBg}">
     <div class="nav-body" :class="items.container ? 'container-fluid' : 'container'">
       <div class="vue-nav-brand">Logo</div>
 
-
+      <!--Nav list-->
       <ul class="vue-nav-list" v-for="item in items" :key="item.routes">
         <li v-for="each in item.routes" :key="each.name">
           <router-link :to="{name: each.routeName}" class="">
@@ -12,6 +11,13 @@
           </router-link>
         </li>
       </ul>
+
+<!--Nav toggler-->
+      <button type="button" class="nav-toggler">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </button>
 
     </div>
 
@@ -23,15 +29,15 @@ export default {
   name: 'Nav-bar',
   props: {
     items: [],
-    navBg:{
-      type:String,
-      default:'#fff',
+    navBg: {
+      type: String,
+      default: '#fff',
     }
 
   },
 
   mounted() {
-    this.items.forEach((v) =>{
+    this.items.forEach((v) => {
       console.log(v.routes)
     })
   }
